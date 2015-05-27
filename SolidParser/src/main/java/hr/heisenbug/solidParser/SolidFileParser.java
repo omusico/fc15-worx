9bc9db674e84a6150294d2c6f1aea48269fd71f8
vlazar- <vlazar@outlook.com>
2015-05-27 15:39:34 +0200

    SolidParser done

--- SolidParser/src/main/java/hr/heisenbug/solidParser/SolidFileParser.java
+++ SolidParser/src/main/java/hr/heisenbug/solidParser/SolidFileParser.java
@@ -1,54 +1,62 @@
 package hr.heisenbug.solidParser;
 
+import com.sun.org.apache.xpath.internal.operations.*;
 import org.apache.poi.poifs.filesystem.*;
 
 import java.io.*;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by Viktor on 24/05/2015.
+ * SolidWorks .sldprt and .sldasm file parser
+ * Extracts PreviewPNG and Header2 stream
+ *
+ * Created by Viktor on 27/05/2015.
  */
-public class SolidParser {
+public class SolidFileParser {
+    String fileName;
+    String solidName;
+    String finalPreviewPath;
+    final String solidPreviewStream = "PreviewPNG";
+    final String solidHeaderStream = "Header2";
 
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-       /* SoldFileOpen sfo = new SoldFileOpen("C:\\Vrotor s utorom1.SLDASM");
-        sfo.openFile();*/
-        String solidFileName = "G:\\Vrotor s utorom1.SLDASM";
-        String solidName = "Vrotor s utorom1.SLDASM";
-        String solidPreviewStream = "PreviewPNG";
-        String solidHeaderStream = "Header2";
+    public SolidFileParser() {
+    }
 
-        SolidFileParser sfp = new SolidFileParser(solidFileName);
-        sfp.parseSolidFile();
+    public SolidFileParser(String fileName) {
+        this.fileName = fileName;
+    }
 
-        //List<String> solidEntries = new ArrayList<String>();
+    /**
+     * Method for parsing SolidWorks .sldasm or .sldprt file
+     *
+     * @return
+     */
+    public List<String> parseSolidFile() {
+        System.out.println("SolidParser v0.1 by Team Heisenbug IGNITED");
+        solidName = fileName.substring(fileName.lastIndexOf(File.separator) + 1);
+        System.out.println("solidName: " + solidName);
+        List<String> headerDataList = new ArrayList<String>();
+
         try {
-            FileInputStream fis = new FileInputStream(solidFileName);
+            FileInputStream fis = new FileInputStream(fileName);
             POIFSFileSystem pfs = new POIFSFileSystem(fis);
             DirectoryEntry root = pfs.getRoot();
-            //System.out.println("Found Root");
             for (Entry entry : root) {
-                //System.out.println("Found Entry: " + entry.getName());
-                //solidEntries.add(entry.getName());
-                if (entry instanceof DirectoryEntry) {
-                    //this is directory
-                    //recurse into directory
-                    //System.out.println("Entry is directory: " + entry.getName());
-                } else if (entry instanceof DocumentEntry) {
+                if (entry instanceof DocumentEntry) {
                     //entry is document/stream and can be read
-                    //System.out.println("Entry is document/stream: " + entry.getName());
                     if (entry.getName().equals(solidPreviewStream)) {
-                        //System.err.println("SUCCESS: Preview stream found!");
                         //save preview stream to png file
                         DocumentInputStream stream = new DocumentInputStream((DocumentEntry) entry);
                         byte[] content = new byte[stream.available()];
                         stream.read(content);
                         stream.close();
-                        String[] nameBase = solidName.split("\\.");
-                        FileOutputStream fos = new FileOutputStream("G:\\" + nameBase[0] + "_PreviewPNG.png");
+                        String[] nameBase = fileName.split("\\.");
+                        FileOutputStream fos = new FileOutputStream(nameBase[0] + "_PreviewPNG.png");
+                        finalPreviewPath = nameBase[0] + "_PreviewPNG.png";
                         try {
                             fos.write(content);
                         } finally {
@@ -56,9 +64,7 @@ public class SolidParser {
                         }
                     }
                     if (entry.getName().equals(solidHeaderStream)) {
-                        //System.err.println("SUCCESS: Header stream found!");
                         //save header2 file data
-                        //DocumentInputStream stream = new DocumentInputStream((DocumentEntry) entry);
                         DocumentInputStream stream = new DocumentInputStream((DocumentEntry) entry);
                         byte[] content = new byte[stream.available()];
                         stream.read(content);
@@ -66,13 +72,12 @@ public class SolidParser {
 
                         String header2Content = new String(content);
                         String header2Content_ = header2Content.replace("\00", "");
-                        //System.out.println(header2Content_);
 
                         String regex = "([^\\\\]+(.SLDPRT|.sldprt|.SLDASM|.sldasm))";
                         Pattern pattern = Pattern.compile(regex);
                         Matcher matcher = pattern.matcher(header2Content_);
                         while (matcher.find()) {
-                            //System.out.println(matcher.group());
+                            headerDataList.add(matcher.group());
                         }
                         String[] nameBase = solidName.split("\\.");
                         FileOutputStream fos = new FileOutputStream("G:\\" + nameBase[0] + "_Header2");
@@ -82,19 +87,30 @@ public class SolidParser {
                             fos.close();
                         }
                     }
-                } else {
-                    // currently, either an Entry is a DirectoryEntry or a DocumentEntry,
-                    // but in the future, there may be other entry subinterfaces. The
-                    // internal data structure certainly allows for a lot more entry types.
-                    //System.out.println("Entry is something: " + entry.getName());
                 }
-
             }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return headerDataList;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * Getter for PreviewPNG image
+     * @return
+     */
+    public String getFinalPreviewPath() {
+        return finalPreviewPath;
     }
 }
